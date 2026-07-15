55b66065881bf6fc32983a23e0ccdfd962e9cbec
schmidtale <schmidt.trossingen@web.de>
2026-07-15 20:56:16 +0900

    added user_db.php to validate user login

---
+++ model/user_db.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'database.php';
+function is_valid_user_login($username, $password) {
+    global $db;
+    $query = 'SELECT * FROM users WHERE username = :username';
+    $stmt = $db->prepare($query);
+    $stmt->execute([':username' => $username]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user) {
+        return password_verify($password, $user['password']);
+    }
+    return false;
+}
+// run
+// password_hash('secret123', PASSWORD_DEFAULT);
+// to generate hash for user passwords
+?>
