4a656f26ca7b8e03a92ffe36c32a3a89815d4b49
schmidtale <schmidt.trossingen@web.de>
2026-07-15 16:38:16 +0900

    Favorites and Reservation

---
+++ pages/favorites.php
@@ -0,0 +1,10 @@
+<?php
+
+$pageTitle = "Jlook | Favorites";
+$pageCSS = "../assets/css/favorites.css";
+
+include "../includes/header.php";
+
+?>
+
+<!-- Favorites Page -->
---
+++ pages/reservation.php
@@ -0,0 +1,10 @@
+<?php
+
+$pageTitle = "Jlook | Reservation";
+$pageCSS = "../assets/css/reservation.css";
+
+include "../includes/header.php";
+
+?>
+
+<!-- Reservation Page -->
